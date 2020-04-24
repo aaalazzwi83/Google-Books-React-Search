9e561ca55f1c7e7a93bdc77a8b1e528d16396f4f
Ammar Alazzawi <aaalazzawi83@gmail.com>
2020-04-24 11:45:49 -0700

    deploy

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes)
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebooks", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://dbuser:dbpassword@ds341837.mlab.com:41837/heroku_75dqwkmh", { useNewUrlParser: true });
 
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
